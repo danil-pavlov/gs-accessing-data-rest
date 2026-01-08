7d5a76366674d25a6fef8b326ca9a6f1a4e87c5a
Danil Pavlov <danil.pavlov@jetbrains.com>
2026-01-08 17:21:23 +0100

    Cleanup and upgrade to Spring Boot 4.0.1

--- complete/src/test/java/com/example/accessingdatarest/AccessingDataRestApplicationTests.java
+++ complete/src/test/java/com/example/accessingdatarest/AccessingDataRestApplicationTests.java
@@ -1,34 +1,18 @@
-/*
- * Copyright 2016 the original author or authors.
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package com.example.accessingdatarest;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-
 import static org.hamcrest.Matchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static java.util.Objects.requireNonNull;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -68,7 +52,7 @@ public class AccessingDataRestApplicationTests {
 				"{\"firstName\": \"Frodo\", \"lastName\":\"Baggins\"}")).andExpect(
 						status().isCreated()).andReturn();
 
-		String location = mvcResult.getResponse().getHeader("Location");
+		String location = requireNonNull(mvcResult.getResponse().getHeader("Location"));
 		mockMvc.perform(get(location)).andExpect(status().isOk()).andExpect(
 				jsonPath("$.firstName").value("Frodo")).andExpect(
 						jsonPath("$.lastName").value("Baggins"));
@@ -95,7 +79,7 @@ public class AccessingDataRestApplicationTests {
 				"{\"firstName\": \"Frodo\", \"lastName\":\"Baggins\"}")).andExpect(
 						status().isCreated()).andReturn();
 
-		String location = mvcResult.getResponse().getHeader("Location");
+		String location = requireNonNull(mvcResult.getResponse().getHeader("Location"));
 
 		mockMvc.perform(put(location).content(
 				"{\"firstName\": \"Bilbo\", \"lastName\":\"Baggins\"}")).andExpect(
@@ -113,7 +97,7 @@ public class AccessingDataRestApplicationTests {
 				"{\"firstName\": \"Frodo\", \"lastName\":\"Baggins\"}")).andExpect(
 						status().isCreated()).andReturn();
 
-		String location = mvcResult.getResponse().getHeader("Location");
+		String location = requireNonNull(mvcResult.getResponse().getHeader("Location"));
 
 		mockMvc.perform(
 				patch(location).content("{\"firstName\": \"Bilbo Jr.\"}")).andExpect(
@@ -131,7 +115,7 @@ public class AccessingDataRestApplicationTests {
 				"{ \"firstName\": \"Bilbo\", \"lastName\":\"Baggins\"}")).andExpect(
 						status().isCreated()).andReturn();
 
-		String location = mvcResult.getResponse().getHeader("Location");
+		String location = requireNonNull(mvcResult.getResponse().getHeader("Location"));
 		mockMvc.perform(delete(location)).andExpect(status().isNoContent());
 
 		mockMvc.perform(get(location)).andExpect(status().isNotFound());
